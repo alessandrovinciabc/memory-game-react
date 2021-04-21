ae78300e2c9d980d706fbf6487d36425c2b6d4e8
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-21 11:57:37 +0200

    Add effect hook to fetch images, display them in cards

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,17 +5,43 @@ import './App.css';
 import Scoreboard from '../Scoreboard/Scoreboard.js';
 import Card from '../Card/Card.js';
 
+let getImages = async (n) => {
+  let images = [];
+
+  try {
+    while (images.length < n) {
+      let response = await fetch('https://randomfox.ca/floof/');
+      let newImage = await response.json();
+
+      if (images.indexOf(newImage.image) === -1) {
+        images.push(newImage.image);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  return images;
+};
+
 function App() {
   let [score, setScore] = useState(0);
   let [best, setBest] = useState(0);
+  let [cardImages, setCardImages] = useState('');
+
+  useEffect(() => {
+    getImages(5).then((newImages) => {
+      setCardImages(newImages);
+    });
+  }, []);
 
   return (
     <div className="App">
       <Scoreboard score={score} best={best} />
       <div className="Game">
-        <Card />
-        <Card />
-        <Card />
+        <Card img={cardImages[0]} />
+        <Card img={cardImages[1]} />
+        <Card img={cardImages[2]} />
       </div>
       <button className="Button--reset">Clear Highscore</button>
     </div>
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './Card.css';
 
 function Card(props) {
-  return <div className="Card"></div>;
+  return (
+    <div className="Card">
+      <img className="Card__image" src={props.img} alt="" />
+    </div>
+  );
 }
 
 export default Card;
